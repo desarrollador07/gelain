4e27da9681ee605663571960243d59dfd8a581bd
Santiago Rivera <santiagoraika@gmail.com>
2021-11-19 17:31:16 -0500

    se agrego los servicios y la tabla de identificacion de peligros

--- src/app/modules/valor-riesgo/valor-riesgo.module.ts
+++ src/app/modules/valor-riesgo/valor-riesgo.module.ts
@@ -6,7 +6,7 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DataViewModule } from 'primeng/dataview';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
-import { FieldsetModule, CalendarModule, CheckboxModule, ProgressBarModule, ProgressSpinnerModule, CardModule, InputTextModule, ButtonModule, InputTextareaModule, DropdownModule, PanelModule, DialogModule, RadioButtonModule, TabMenuModule, TooltipModule } from 'primeng/primeng';
+import { FieldsetModule, CalendarModule, CheckboxModule, ProgressBarModule, ProgressSpinnerModule, CardModule, InputTextModule, ButtonModule, InputTextareaModule, DropdownModule, PanelModule, DialogModule, RadioButtonModule, TabMenuModule, TooltipModule, TabViewModule } from 'primeng/primeng';
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { ValorRiesgoRoutingModule } from './valor-riesgo-routing.module';
@@ -46,7 +46,8 @@ import { FormValorRiesgoComponent } from '../form-valor-riesgo/form-valor-riesgo
     DialogModule,
     RadioButtonModule,
     TabMenuModule,
-    TooltipModule
+    TooltipModule,
+    TabViewModule
   ]
 })
 export class ValorRiesgoModule { }
