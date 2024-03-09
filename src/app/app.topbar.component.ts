96c50266e4ba85553d8f866123f212dfd0c93ce9
Juan Esteban <qualitycolombia.dvp04@gmail.com>
2024-03-09 08:19:07 -0500

    cambios menores

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -10,7 +10,7 @@ import { Empresa } from './models/empresa.model';
 /*Servicios */
 import { EmpresaService } from './services/empresa.service';
 /*Componente */
-import { AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { ValidacionService } from './services/validacion.service';
 
 @Component({
@@ -20,95 +20,96 @@ import { ValidacionService } from './services/validacion.service';
 })
 export class AppTopBarComponent {
 
-  idEmpresa:number;
-  empresita:Empresa;
-  nombre:string;
+  idEmpresa: number;
+  empresita: Empresa;
+  nombre: string;
   empresa: SelectItem[] = [];
   empresas: Empresa[] = [];
-  empresaSelect:any;
+  empresaSelect: any;
   mobWidth: any;
   imgvalidator: boolean = true;
-  imagen1:any = "https://gelainbienestarlaboral.com/GELAIN/img/usr01.jpg";
-  imagen2:any = "https://gelainbienestarlaboral.com/GELAIN/img/usr02.jpg";
-  imagen3:any = "https://gelainbienestarlaboral.com/GELAIN/img/usr03.jpg";
-  imagenDefecto:any = "assets/layout/images/avatar.png"
-  imagenfin:any;
-  loading:boolean = true;
+  imagen1: any = "https://gelainbienestarlaboral.com/GELAIN/img/usr01.jpg";
+  imagen2: any = "https://gelainbienestarlaboral.com/GELAIN/img/usr02.jpg";
+  imagen3: any = "https://gelainbienestarlaboral.com/GELAIN/img/usr03.jpg";
 
-    constructor(public app: AppComponent,
-                private router: Router,
-                private store: Store<AppState>,
-                private empresaServices: EmpresaService,
-                private validacionService: ValidacionService) {
-      this.validacionService.recargarPagina();
-      this.onResize();
-      this.nombre = sessionStorage.getItem("user");
-      this.idEmpresa = Number(sessionStorage.getItem("idEmpresa"));
-       if (sessionStorage.getItem("user")==="LINA") {
-        this.imagenfin = this.imagen1;
-      } else if (sessionStorage.getItem("user")==="JENNIFER") {
-        this.imagenfin = this.imagen2;
-      }else if(sessionStorage.getItem("user")==="MONICA"){
-        this.imagenfin = this.imagen3;
-      }else{
-        this.imagenfin = this.imagenDefecto;
-      }
+  imagenDefecto: any = "assets/layout/images/avatar.png"
+  imagenfin: any;
+  loading: boolean = true;
+
+  constructor(public app: AppComponent,
+    private router: Router,
+    private store: Store<AppState>,
+    private empresaServices: EmpresaService,
+    private validacionService: ValidacionService) {
+    this.validacionService.recargarPagina();
+    this.onResize();
+    this.nombre = sessionStorage.getItem("user");
+    this.idEmpresa = Number(sessionStorage.getItem("idEmpresa"));
+    if (sessionStorage.getItem("user") === "LINA") {
+      this.imagenfin = this.imagen1;
+    } else if (sessionStorage.getItem("user") === "JENNIFER") {
+      this.imagenfin = this.imagen2;
+    } else if (sessionStorage.getItem("user") === "MONICA") {
+      this.imagenfin = this.imagen3;
+    } else {
+      this.imagenfin = this.imagenDefecto;
     }
+  }
 
-    @HostListener("window:resize", ["$event"])
-    onResize() {
-        this.mobWidth = window.innerWidth;
+  @HostListener("window:resize", ["$event"])
+  onResize() {
+    this.mobWidth = window.innerWidth;
 
-        if (this.mobWidth <= 1024) {
-            this.imgvalidator = false;
-        } else {
-            this.imgvalidator = true;
-        }
+    if (this.mobWidth <= 1024) {
+      this.imgvalidator = false;
+    } else {
+      this.imgvalidator = true;
     }
+  }
 
   ngOnInit() {
 
     /*Consulta de las empresas */
     this.consultaEmpresas();
-    
+
   }
 
-    buscarActualizarData(){
+  buscarActualizarData() {
 
-      this.store.dispatch(empresasActions.selectEmpresa({ id: parseInt(this.empresaSelect) }));
-      let select = this.empresa.find(el => el.value == this.empresaSelect);
-      sessionStorage.setItem("nombreEmpresa",select.label);
-      sessionStorage.setItem('idEmpresa', this.empresaSelect);
-      // this.router.navigate(["main/dashboard"]);
+    this.store.dispatch(empresasActions.selectEmpresa({ id: parseInt(this.empresaSelect) }));
+    let select = this.empresa.find(el => el.value == this.empresaSelect);
+    sessionStorage.setItem("nombreEmpresa", select.label);
+    sessionStorage.setItem('idEmpresa', this.empresaSelect);
+    // this.router.navigate(["main/dashboard"]);
 
-    }
+  }
 
-    salir(){
-      sessionStorage.clear();
-      this.router.navigate(["/login"]);
-    }
+  salir() {
+    sessionStorage.clear();
+    this.router.navigate(["/login"]);
+  }
 
-    async consultaEmpresas(){
-        this.empresa = [];
-        await this.empresaServices.getEmpresa().toPromise().then((data:any) => {
+  async consultaEmpresas() {
+    this.empresa = [];
+    await this.empresaServices.getEmpresa().toPromise().then((data: any) => {
 
-          this.empresas = data;
-          this.empresas.map(x =>{
-            this.empresa.push({
-              label:x.empnombre,
-              value:x.empid
-            });
-          });
-          this.store.dispatch(
-            empresasActions.addEmpresas({ list: data })
-          );
-          this.loading = false;
+      this.empresas = data;
+      this.empresas.map(x => {
+        this.empresa.push({
+          label: x.empnombre,
+          value: x.empid
         });
+      });
+      this.store.dispatch(
+        empresasActions.addEmpresas({ list: data })
+      );
+      this.loading = false;
+    });
 
-        if (this.idEmpresa !== 0) {
-          this.empresaSelect = this.idEmpresa;
-        }
-        this.store.dispatch(empresasActions.selectEmpresa({ id: this.empresaSelect }));
+    if (this.idEmpresa !== 0) {
+      this.empresaSelect = this.idEmpresa;
     }
+    this.store.dispatch(empresasActions.selectEmpresa({ id: this.empresaSelect }));
+  }
 
 }
